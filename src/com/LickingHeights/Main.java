1be5d3612de054e31db00a9d1413205aca1776a9
Brady Mann <Football3320>
2019-02-07 14:12:47 -0500

    Imported the Scanner and declared keyboard

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,10 +1,13 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	String greeting, name;
     int inches, pounds, meters, kilograms;
+    Scanner keyboard;
 
 
 
