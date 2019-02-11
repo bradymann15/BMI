962fd0011beab668264223725b683d7125c321d5
Brady Mann <Football3320>
2019-02-11 14:16:12 -0500

    Converted ounces to kilograms, and initialized kilograms

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -20,15 +20,18 @@ public class Main {
     System.out.println("What is your height in inches?");
     inches = keyboard.nextInt();
     System.out.print("Now, we will take your height in inches (" + inches);
-    System.out.println(") and convert it to centimeters for you.");
+    System.out.println(") and convert it into centimeters for you.");
     System.out.println(inches*2.54);
     System.out.println("Type the number you get down below.");
     centimeters = keyboard.nextInt();
     System.out.println("That number will be your height in centimeters.");
     System.out.println("What is your weight in ounces?");
     ounces = keyboard.nextInt();
-
-
+    System.out.print("Now, we will take your weight in ounces (" + ounces);
+    System.out.println(") and convert it into kilograms for you.");
+    System.out.println(ounces/35.274);
+    System.out.println("Type the number you get down below. This will be your weight in kilograms.");
+    kilograms = keyboard.nextInt();
 
 
 
