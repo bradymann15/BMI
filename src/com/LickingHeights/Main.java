202abeb5e309176cd1fc5c4f77d07aaed375ccd0
Brady Mann <Football3320>
2019-02-11 13:37:18 -0500

    Added headings for the declared words and initialization sections

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class Main {
 
+    //declare
     public static void main(String[] args) {
 	String greeting, name;
     int inches, pounds, meters, kilograms;
     Scanner keyboard;
 
+    //initialize + code
     greeting = "Hello there";
     keyboard = new Scanner(System.in);
     
